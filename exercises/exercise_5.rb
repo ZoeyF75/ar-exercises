9ee70c34b89c76af21397ab9882f82dc10a0396e
Zoey Farago <zf.zoodle@gmail.com>
2021-03-02 01:16:39 +0000

    Avergage and Count active records

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,8 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total = Store.sum(:annual_revenue)
+@average = Store.average(:annual_revenue)
+@annualSalesMill = Store.where("annual_revenue > 1000000").count
+
+puts @total, @average, @annualSalesMill
